58168b645138bb6bbcffd0aa7c0c78bccb9022cc
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2021-06-14 09:22:23 +0300

    chore: автоматический запуск docker-контейнеров в окружении разработки

--- config/gulp/serve.js
+++ config/gulp/serve.js
@@ -1,4 +1,5 @@
-const { watch } = require('gulp')
+const { watch, series } = require('gulp')
+const { spawn } = require('child_process')
 
 const { copy, copyGlobs } = require('./copy')
 const remove = require('./remove').default
@@ -6,7 +7,13 @@ const styles = require('./styles').default
 const scripts = require('./scripts').default
 const { srcPath, themeSlug} = require('./paths')
 
-function serve() {
+require('./utils/windows-sigint')
+
+function serve(cb) {
+  process.on('SIGINT', function() {
+    cb()
+  })
+
   const watcher = watch(copyGlobs, copy)
   watcher.on('unlink', remove)
 
@@ -17,4 +24,17 @@ function serve() {
   watch(scriptsGlob, scripts)
 }
 
-exports.default = serve
+function runDockerContainers(cb) {
+  spawn('docker', ['compose', 'up', '-d'], { stdio: 'inherit' })
+    .on('close', cb)
+}
+
+function stopDockerContainers(cb) {
+  spawn('docker', ['compose', 'down'], { stdio: 'inherit' })
+    .on('close', function() {
+      cb()
+      process.exit()
+    })
+}
+
+exports.default = series(runDockerContainers, serve, stopDockerContainers)
